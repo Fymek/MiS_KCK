4ea851cc86b43f855af449b91b7e5675397a3bb4
BToruch <235999@edu.p.lodz.pl>
2022-05-12 13:47:27 +0200

    Koniec zajęć lab1

--- src/main/java/com/example/mis_kck/CreatorApplication.java
+++ src/main/java/com/example/mis_kck/CreatorApplication.java
@@ -1,20 +1,23 @@
 package com.example.mis_kck;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CreatorApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("kreator.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CreatorApplication.class.getResource("kreator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Widły i Kapcie!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/mis_kck/CreatorController.java
@@ -0,0 +1,36 @@
+package com.example.mis_kck;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreatorController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private ImageView confirmButton;
+
+    @FXML
+    private Pane panel;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onConfirmButtonClick(Stage stage) throws IOException {
+        panel = new Pane(getClass().getResource("walka.fxml"));
+        Scene scene = new Scene(panel);
+        stage.setTitle("Widły i Kapcie!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
