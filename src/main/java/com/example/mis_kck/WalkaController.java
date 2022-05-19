5678d7b37d56faee34f1708fc29eda8d6c11436b
Fymek <Fymek01@gmail.com>
2022-05-19 12:56:06 +0200

    dodanie WalkaController

--- src/main/java/com/example/mis_kck/WalkaController.java
+++ src/main/java/com/example/mis_kck/WalkaController.java
@@ -1,17 +1,40 @@
 package com.example.mis_kck;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class WalkaController {
+    @FXML
+    private Button sleepButton;
+    @FXML
+    private Button atackQuick;
+    @FXML
+    private Button atackMedium;
+    @FXML
+    private Button atackPower;
+    @FXML
+    private Button blockButton;
+    @FXML
+    private Text hpbar;
+    @FXML
+    private Text hpbar1;
+    @FXML
+    private Text manabar;
+    @FXML
+    private Text manabar1;
     Postac postac1,postac2;
     int tura;
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//
+//    }
 }
